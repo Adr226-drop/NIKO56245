cc6542b6a45e55facdca3eb6cadfe34db16f51f1
Adr226-drop <pedrozaromeroadriannicolas@cetis50cdmx.com>
2024-09-10 19:14:20 -0600

    la app2

---
+++ App02/app.py
@@ -0,0 +1,13 @@
+import flet as ft
+
+
+def main(page: ft.page):
+    page.title="¿Me perdonas?"
+    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
+    Page.bgcolor="blue"
+    
+    lbl1=ft.Text("¿Me perdonas?",
+                style=ft.TextStyle(size=40,weight="blod"))
+    
+    Ing1
